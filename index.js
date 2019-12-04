f70ee906739728991b1bbeeb38586b212f837cf7
IsaiahFlax <flax.isaiah@gmail.com>
2019-12-03 19:46:17 -0700

    Update index.js
    5 passing 19 failing

--- index.js
+++ index.js
@@ -41,9 +41,9 @@ function addNumbers(num1, num2) {
 */
 function makePersonObject(id,name,email) {
   return {
-    id: id,
-    name: name,
-    email: email
+    id,
+    name,
+    email
   };
 }
 
@@ -61,8 +61,9 @@ function makePersonObject(id,name,email) {
  * passing { id: 1, name: 'Leia', email: 'leia@leia.com` } as the argument,
  * the returned value should look like `Hello, my name is Leia`.
 */
-function getName(/* code here */) {
-  /* code here */
+function getName(objectContainingName) {
+  return("Hello, my name is " +objectContainingName.name);
+  
 }
 
 /**
@@ -78,9 +79,19 @@ function getName(/* code here */) {
  *         and returns a string like `Hello, my name is {name}`.
  *         where `{name}` is the name passed into `makeSmartPerson`.
 */
-function makeSmartPerson(/* code here */) {
-  /* code here */
-}
+function makeSmartPerson(singleNameArgument) {  
+  return {
+    name:singleNameArgument,
+    sum : function (num1, num2) {
+      return num1 + num2; },
+    speak: function () {return("Hello, my name is " +singleNameArgument.name);}
+    }
+
+    }
+
+
+  
+
 
 
 
