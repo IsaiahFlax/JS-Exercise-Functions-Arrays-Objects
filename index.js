17e6678967d4d9d69226e5b6a1a5b3a33d614139
IsaiahFlax <flax.isaiah@gmail.com>
2019-12-04 20:19:18 -0700

    Update index.js
    21 passing 3 failing

--- index.js
+++ index.js
@@ -304,9 +304,9 @@ function getGermanCars(inventory) {
  *   return num * 2
  * }
 */
-const sum = null; // code here!
-const addFive = null; // code here!
-const argTimesTwo = null; // code here!
+const sum = (num1, num2) => { return num1 + num2; }; // code here!
+const addFive = num => {return num + 5; }; // code here!
+const argTimesTwo = num => {return num * 2; };; // code here!
 
 /**
  * ### Challenge `carMaker`
