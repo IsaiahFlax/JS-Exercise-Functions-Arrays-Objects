d0a4b5bb3ffbb46989013434cfb9bcb9541b7c00
IsaiahFlax <flax.isaiah@gmail.com>
2019-12-04 19:27:20 -0700

    Update index.js
    13 passing 11 failing

--- index.js
+++ index.js
@@ -206,8 +206,9 @@ function getCarInfoById(inventory, id) {
  *     (1) an array which is an inventory of cars like the one inside /data/inventory.js.
  * sortCarInventory returns an inventory that is sorted by car_model, ascending [A-Z].
 */
-function sortCarInventory(/* code here */) {
-  /* code here */
+function sortCarInventory(inventory) {
+  inventorySortedByCarModel = inventory.sort((a, b) => a.car_model.localeCompare(b.car_model))
+return inventorySortedByCarModel;
 }
 
 /**
